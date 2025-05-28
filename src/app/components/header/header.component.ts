04d8a8a9a79c7fa252b5d87512b23e4f4da1ed83
Bruno Toniatti <toniattibruno08@gmail.com>
2025-05-28 19:56:05 -0300

    Adicionando imagem dentro do componente header e configurando para aparecer na nossa home

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
+import { NgOptimizedImage } from '@angular/common'
+
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
